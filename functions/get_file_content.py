9352b56f456724b7be12f1415f4adcc0bab3dfb6
Herman-96 <herman444@live.se>
2025-07-21 17:37:30 +0200

    Finished the project

---
+++ functions/get_file_content.py
@@ -0,0 +1,36 @@
+import os
+from config import MAX_CHARS
+from google.genai import types
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+def get_file_content(working_directory, file_path):
+    abs_file_path = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_file_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+
+    try:
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
+    except Exception as e:
+        return f"Error: {str(e)}"
